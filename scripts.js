252ab87cd7bdd93d685fe91e76b3cb022fd9214a
D.I.P Lakshan <onlineipl18@gmail.com>
2022-02-08 17:44:03 +0530

    with returnt not return

--- scripts.js
+++ scripts.js
@@ -1,35 +1,44 @@
+//function is a reusable peace of code that does specific thing
 
-
-var match1,match2,match3,avgSri,avgEng;
-
-// Sri lanka avarage
-match1 = parseInt(prompt("Sri lanaka First match score"));
-match2 = parseInt(prompt("Sri lanaka Second match score"));
-match3 = parseInt(prompt("Sri lanaka Third match score"));
-
-avgSri = (match1 + match2 + match3) / 3;
-
-console.log(" Sri lanka team " + avgSri);
-
-// Engaland team avarage
-match1 = parseInt(prompt("Engaland First match score"));
-match2 = parseInt(prompt("Engaland Second match score"));
-match3 = parseInt(prompt("Engaland Third match score"));
-
-avgEng = (match1 + match2 + match3) / 3;
-
-console.log(" Engalanda team " + avgEng);
-
-if (avgSri > avgEng){
-    alert("Winner team is Sri lankan  .Avrrage Score is " + avgSri);
-    console.log("Winner team is Sri lankan  .Avrrage Score is " + avgSri);
-    
+//without return
+function addNumbers(num1,num2){
+    var ans = num1 + num2;
+    console.log('Addition : ' + ans);
 }
-else if(avgEng > avgSri){
-    alert("Winner team is Engaland .Avrrage Score is " + avgEng);
-    console.log("Winner team is Engaland .Avrrage Score is " + avgEng);
+
+//with return
+function subNumbers(num1,num2){
+  
+    return num1 - num2;
+   
 }
-else{
-    alert("Match is draw" );
-    console.log("Match is draw" );
+
+//invoke function
+
+addNumbers(5,6);
+var sub = subNumbers(10,5);
+
+console.log('Subtraction : ' + sub);
+
+
+
+//............................................
+
+//Define
+function calculateAge(birthYear){
+    return 2022 - birthYear;
 }
+
+//invoke
+console.log("Your age is : " + calculateAge(1998) + " Years old." );
+
+function retimentyear(fisrtName,birthYear){
+    var age = calculateAge(birthYear);
+    var retire = 60 - age;
+
+    console.log(fisrtName + " retire in " + retire + " year in " + (birthYear + age + retire)  );
+
+}
+
+//invoke 
+retimentyear('ishanka',1964);
