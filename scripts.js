cfca2dc42531762810eb4f0591f8b872e815cdb7
D.I.P Lakshan <onlineipl18@gmail.com>
2022-02-10 10:52:58 +0530

    Tip Calculator

--- scripts.js
+++ scripts.js
@@ -1,47 +1,28 @@
-//array
-//array is single veriable that used for store diffrent elements. its often use when we want to list of elements and access them by single veriable.
+var tips = [];
+var amount = [];
 
-// way to difine array
-var student =["ishanka" , "pramidu", "lakshan"];
-var marks = new Array(60,55,25);
+function bilCalulator(bill){
+    var tip;
+    if(bill < 1000){
+        tip = bill * 20 /100;
+    }
+    else if(bill >= 1000 && bill <= 1500){
+        tip = bill * 15 /100;
+    }
+    else{
+        tip = bill * 10 /100;
+    }
 
-//print array 
-console.log(student);
-console.log(marks);
-console.log(student[2] + ' got ' + marks[2] + ' marks for Maths' );
+    tips.push(tip);
+    amount.push(bill + tip);
+   
+    return tip;
+}
 
-//arry mutation
-student[0] = 'Lalith' 
-student[student.length] = "Chamara"  // its add new name in end of the  array
-console.log(student); // in the console lalith is printed instend of ishanka
+console.log("1 - TIP:" + bilCalulator(1230) )
+console.log("2 - TIP:" + bilCalulator(982) )
+console.log("3 - TIP:" + bilCalulator(1640) )
 
-//Different type of array
-var std1 = ["kamal","Perera","Civil",23,true];
+console.log(tips)
+console.log(amount)
 
-console.log(std1);
-console.log(typeof(std1[3])); //its give datatype of 3 element (boolen)
-
-//Add elements
-std1.push('green'); // add as last element 
-std1.unshift('MR'); // add as first element 
-console.log(std1);
-
-//Remove elements
-std1.pop(); //Remove as last element 
-std1.shift(); //Remove as first element 
-console.log(std1);
-
-
-//get index of an element
-console.log(std1.indexOf(22))
-
-var inAgriStudent = std1.indexOf("Agri") === -1 ? std1[0] + 'is not Agri Student' :
-std1[0] +  'is a Agri student';
-
-console.log(inAgriStudent);
-
-// var checkInt = std1.indexOf(22) === -1 ? 'is not 22 ' : 'is 22'
-// console.log(checkInt);
-
-var checkInt = typeof(std1[3]) === "number" ? 'is int ' : 'is not int'
-console.log(checkInt);
