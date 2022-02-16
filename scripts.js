ad8ac397f5c5ecc1633b5f8b283782d40134a236
D.I.P Lakshan <onlineipl18@gmail.com>
2022-02-16 20:07:10 +0530

    Error Handaling

--- scripts.js
+++ scripts.js
@@ -1,56 +1,34 @@
- for(var i = 5 ; i >= 0 ; i--){
-     console.log(i);
- }
+/// try catch Error handling
 
- //itarete an array
- var student = ["ishanka ", "pramidu","Lakshan"];
-
- for(var i = 0 ; i < student.length ; i++){
-    console.log(student[i]);
- }
-
- //for In loop
-
- var person = {frame:"ishanka",lname:'Pramidu',age:24}
-
- for(var i in person){
-     console.log(person[i]);
- }
-
- //for of looop
-var fullName = "Ishanka"
-
-for(var j of fullName){
-    console.log(j);
+function largestNumber(a,b,c){
+   try{
+        if(typeof(a) !== 'number' || typeof(b) !== 'number' || typeof(c) !== 'number'){
+            console.log("Error : Please Enter only Numbers.");
+        }
+        else if(a == b || a == c || b == c ){
+            console.log("Error : Please Enter diffirent Numbers");
+        }
+        else{
+            if(a > b && a > c){
+                console.log("A is Largest Number!");
+            }
+            else if(b > a && b > c){
+                console.log("B is Largest Number!");
+            }
+            else if(c > a && c > b){
+                console.log("C is Largest Number!");
+            }
+            else{
+                console.log("Invalid Inputs!");
+            }
+        }
+   }
+   catch(err){
+    console.log(err);
+   }
+   finally{
+    console.log("You inputs values are " + a + " " + b + " and " + c);
+   }
 }
 
-//While looop
-var marks =[33,55,66,88,66,99]
-var k = 0;
-while(k < marks.length){
-    console.log(marks[k]);
-    k++;
-}
-
-//do while loop
-var l = 10;
-do{
-    console.log(l);
-    l--;
-}while(l >= 0);
-
-// // continue and break
-
-var data = ["ishanka","Kaduwela",1996,"math",true,"chocolate"];
-
-//continue
-for(var p=0 ; p<data.length ;p++){
-    if(typeof(data[p]) !== 'string')continue;
-    console.log(data[p]);
-}
-
-//break
-for(var p=0 ; p<data.length ;p++){
-    if(typeof(data[p]) !== 'string')break;
-    console.log(data[p]);
-}
+largestNumber(1,3,3);
