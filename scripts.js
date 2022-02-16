7ded3d6816265ea5f4e78b6f5b564b43684c4101
D.I.P Lakshan <onlineipl18@gmail.com>
2022-02-16 22:38:05 +0530

    Scope

--- scripts.js
+++ scripts.js
@@ -1,34 +1,22 @@
-/// try catch Error handling
 
-function largestNumber(a,b,c){
-   try{
-        if(typeof(a) !== 'number' || typeof(b) !== 'number' || typeof(c) !== 'number'){
-            console.log("Error : Please Enter only Numbers.");
-        }
-        else if(a == b || a == c || b == c ){
-            console.log("Error : Please Enter diffirent Numbers");
-        }
-        else{
-            if(a > b && a > c){
-                console.log("A is Largest Number!");
-            }
-            else if(b > a && b > c){
-                console.log("B is Largest Number!");
-            }
-            else if(c > a && c > b){
-                console.log("C is Largest Number!");
-            }
-            else{
-                console.log("Invalid Inputs!");
-            }
-        }
-   }
-   catch(err){
-    console.log(err);
-   }
-   finally{
-    console.log("You inputs values are " + a + " " + b + " and " + c);
-   }
+//Local variable
+
+function addNum(){
+    var a = 20;
+    var b = 10;
+
+    console.log(a+b);
 }
 
-largestNumber(1,3,3);
+
+//Golbal variabale
+
+var c = 20;
+function showsum(){
+
+    console.log(c);
+     c = 30;
+}
+
+showsum();
+showsum();
